456d45c397ad14f6bc03d53770800ea6eb05639f
Ninoh-FOX <ninohfox@gmail.com>
2018-03-17 09:31:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <psp2/types.h>
+#include <psp2/apputil.h>
 #include <psp2/kernel/processmgr.h>
 #include <psp2/ctrl.h>
 #include <psp2/io/fcntl.h>
@@ -88,7 +90,7 @@ int main()
 								sceKernelExitProcess(0);
 								break;
 							case 1:
-								ret = sceAppMgrLaunchApp("vs0:vsh/shell/shell.self", NULL, NULL);//DOESNT WORK
+								ret = sceAppMgrLoadExec(const char "vs0:vsh/shell/shell.self", NULL, NULL);//DOESNT WORK
 								sprintf(con_data, "Cargando shell.self: %d ",ret);
 								strcat(log_text,con_data);
 								select_menu();
@@ -174,7 +176,7 @@ int main()
 					case 4:
 						switch (sub_selected){
 							case 0://Start vitashell
-								ret = sceAppMgrLaunchApp("ux0:app/VITASHELL/eboot.bin", NULL, NULL);//DOESNT WORK
+								ret = sceAppMgrLoadExec(const char "ux0:app/VITASHELL/eboot.bin", NULL, NULL);//DOESNT WORK
 								sprintf(con_data, "Cargando VITASHELL: %d ", ret);
 								strcat(log_text,con_data);
 								break;
