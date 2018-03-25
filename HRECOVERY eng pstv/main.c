7dce5d53047c03781e2fe2e06c9d2add1c60a983
Ninoh-FOX <ninohfox@gmail.com>
2018-03-25 21:31:36 +0200

    version

--- HRECOVERY eng pstv/main.c
+++ HRECOVERY eng pstv/main.c
@@ -59,7 +59,7 @@ void select_menu(){
 	psvDebugScreenClear(0x00000000);
 	psvDebugScreenSetFgColor(COLOR_YELLOW);
 	psvDebugScreenPrintf("Ninoh-FOX            --[Menu Recovery]--                         \n");
-	psvDebugScreenPrintf("                     --[HaiMenu v1.02]--            EOL.net      \n");
+	psvDebugScreenPrintf("                     --[HaiMenu v1.03]--            EOL.net      \n");
 	psvDebugScreenSetFgColor(COLOR_GREY);
 	psvDebugScreenPrintf("Opcion(%d,%d): %s.\n\n",selected,sub_selected,menu_options[selected][sub_selected]);
 	psvDebugScreenSetFgColor(COLOR_GREY);
